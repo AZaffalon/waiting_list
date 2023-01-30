6880916959686d38179b95a32ebed8ffe647a4a5
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2023-01-30 16:12:17 +0100

    Add scopes for unconfirmed, confirmed and accepted

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,6 +4,9 @@ class Request < ApplicationRecord
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   validates_format_of :phone_number, with: /^(?:(?:(?:\+|00)33[ ]?(?:\(0\)[ ]?)?)|0){1}[1-9]{1}([ .-]?)(?:\d{2}\1?){3}\d{2}$/i, on: :create, multiline: true
 
+  scope :unconfirmed, -> { where(email_confirmation: false) }
+  scope :confirmed, -> { where(email_confirmation: true) }
+  scope :accepted, -> { where(accepted: true) }
 
   # Accept a request
   def accept!
