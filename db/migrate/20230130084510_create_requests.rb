cddaf07ec3e637f995316acafc4650ccf337e872
Alexis Zaffalon <alexiszaffalon@live.fr>
2024-07-04 10:56:07 +0200

    Changes string to text to prevent futur problem with data that are more than 255 characters long

--- db/migrate/20230130084510_create_requests.rb
+++ db/migrate/20230130084510_create_requests.rb
@@ -1,8 +1,8 @@
 class CreateRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :requests do |t|
-      t.string :name,                 null: false
-      t.string :email,                null:false
+      t.text :name,                 null: false
+      t.text :email,                null: false
       t.boolean :email_confirmation,  default: false
       t.integer :phone_number,        null: false
       t.text :biography,              null: false
